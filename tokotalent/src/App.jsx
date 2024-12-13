bfdb2fb98dd61008563b24fe9cf9dab0a33007b1
blackcatin <umifirlana4@gmail.com>
2024-12-13 21:10:31 +0700

    update url

--- tokotalent/src/App.jsx
+++ tokotalent/src/App.jsx
@@ -1,18 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Dishes from "./components/Dishes";
 import Footer from "./components/Footer";
 
 const App = () => {
-  const [cart, setCart] = useState([]); 
-  const [cartOpen, setCartOpen] = useState(false); 
-  return (
-<div className="min-h-screen flex flex-col">
-  <Navbar cart={cart} setCart={setCart} setCartOpen={setCartOpen} />
-  <Dishes cart={cart} setCart={setCart} setCartOpen={setCartOpen} />
-  <Footer cart={cart} setCart={setCart} setCartOpen={setCartOpen} />
-</div>
+  const [dataMenu, setDataMovies] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [cartOpen, setCartOpen] = useState(false);
 
+  
+  async function getData() {
+    const url = "https://fossil-atlantic-rooster.glitch.me/menu"; 
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log(json);
+
+      setDataMovies(json); 
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getData(); 
+  }, []); 
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar cart={cart} setCart={setCart} setCartOpen={setCartOpen} />
+      <Dishes
+        cart={cart}
+        setCart={setCart}
+        setCartOpen={setCartOpen}
+        dataMenu={dataMenu} 
+      />
+      <Footer cart={cart} setCart={setCart} setCartOpen={setCartOpen} />
+    </div>
   );
 };
 
